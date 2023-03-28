a64e116ec8e04a788eba911508906e2b68717457
Julia Ma <juliama@MacBook-Air-von-Julia.local>
2023-03-28 20:09:05 +0200

    hand in cpp00

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: jmatheis <jmatheis@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 13:05:18 by jmatheis          #+#    #+#             */
-/*   Updated: 2023/03/28 13:53:52 by jmatheis         ###   ########.fr       */
+/*   Updated: 2023/03/28 20:08:27 by jmatheis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-
-
 Account::Account(int initial_deposit)
 {
 	_accountIndex = _nbAccounts;
 	_displayTimestamp();
-	std::cout <<"index:" << _accountIndex
-			<<";amount:" << initial_deposit
-			<<";created" << std::endl;
+	std::cout << "index:" << _accountIndex
+			<< ";amount:" << initial_deposit
+			<< ";created" << std::endl;
 	_amount = initial_deposit;
 	_totalAmount += _amount;
 	_nbAccounts++;
@@ -35,33 +33,34 @@ Account::Account(int initial_deposit)
 Account::~Account()
 {
 	_displayTimestamp();
-	std::cout <<"index:" << _accountIndex
-			<<";amount:" << checkAmount()
-			<<";closed" << std::endl;
+	std::cout << "index:" << _accountIndex
+			<< ";amount:" << checkAmount()
+			<< ";closed" << std::endl;
 }
 
+// locatime years since 1900
 void Account::_displayTimestamp()
 {
-	time_t now = NULL;
+	time_t now;
 	std::time(&now);
 
-	tm *ltm = localtime(&now);
-	std::cout<<"["<< 1900 + ltm->tm_year;
-	if (ltm->tm_mon < 10)
+	tm *timeunit = localtime(&now);
+	std::cout<<"["<< 1900 + timeunit->tm_year;
+	if (timeunit->tm_mon < 10)
 		std::cout << "0";
-	std::cout << 1 + ltm->tm_mon;
-	if (ltm->tm_mday < 10)
+	std::cout << 1 + timeunit->tm_mon;
+	if (timeunit->tm_mday < 10)
 		std::cout << "0";
-	std::cout << ltm->tm_mday << "_";
-	if (ltm->tm_hour < 10)
+	std::cout << timeunit->tm_mday << "_";
+	if (timeunit->tm_hour < 10)
 		std::cout << "0";
-	std::cout << ltm->tm_hour;
-	if (ltm->tm_min < 10)
+	std::cout << timeunit->tm_hour;
+	if (timeunit->tm_min < 10)
 		std::cout << "0";
-	std::cout << ltm->tm_min;
-	if (ltm->tm_sec < 10)
+	std::cout << timeunit->tm_min;
+	if (timeunit->tm_sec < 10)
 		std::cout << "0";
-	std::cout << ltm->tm_sec << "] ";
+	std::cout << timeunit->tm_sec << "] ";
 }
 
 int Account::getNbAccounts()
@@ -89,6 +88,7 @@ int Account::checkAmount( void ) const
 	return(_amount);
 }
 
+// total numbers
 void Account::displayAccountsInfos()
 {
 	_displayTimestamp();
@@ -101,12 +101,11 @@ void Account::displayAccountsInfos()
 void Account::makeDeposit( int deposit )
 {
 	_nbDeposits = 1;
-	int	currentamount = checkAmount();
 	_displayTimestamp();
 	std::cout <<"index:" <<  _accountIndex
-			<<";p_amount:" << currentamount
+			<<";p_amount:" << checkAmount()
 			<<";deposit:" << deposit
-			<<";amount:" << currentamount + deposit
+			<<";amount:" << checkAmount() + deposit
 			<<";nb_deposits:" << _nbDeposits << std::endl;
 	_amount += deposit;
 	_totalAmount += deposit;
@@ -115,27 +114,26 @@ void Account::makeDeposit( int deposit )
 
 bool Account::makeWithdrawal( int withdrawal )
 {
-	int	currentamount = checkAmount();
 	_displayTimestamp();
 	std::cout <<"index:" << _accountIndex
 			<<";p_amount:" << checkAmount()
 			<<";withdrawal:";
-	if (withdrawal <= currentamount)
+	if (withdrawal <= checkAmount())
 	{
 		_nbWithdrawals = 1;
-		std::cout << withdrawal;
 		_amount -= withdrawal;
 		_totalAmount -= withdrawal;
-		std::cout << ";amount:" << currentamount - withdrawal
-				<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 		_totalNbWithdrawals++;
+		std::cout << withdrawal
+				<< ";amount:" << checkAmount()
+				<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 		return (true);
 	}
-	std::cout <<"refused"<<std::endl;
+	std::cout << "refused" << std::endl;
 	return (false);
 }
 
-
+// for every index
 void Account::displayStatus( void ) const
 {
 	_displayTimestamp();
