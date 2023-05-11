d2114de5de8fd5382c22b341550df0bdfefc016d
jmatheis <jmatheis@student.42heilbronn.de>
2023-05-11 10:09:17 +0200

    cop assignment cpp05 ex00 fixed

---
+++ cpp_05/ex01New/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+
+#define RESET       "\033[0m"               /* Reset */
+#define RED         "\033[31m"              /* Red */
+#define GREEN       "\033[32m"              /* Green */
+#define YELLOW      "\033[33m"              /* Yellow */
+#define PURPLE      "\033[35m"              /* Purple */
+
+class Bureaucrat
+{
+    public:
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &copyclass); //Copy Constructor
+		Bureaucrat& operator= (const Bureaucrat& copyop); //copy assignment operator
+		~Bureaucrat(); //Destructor
+
+		// GETTER
+		const std::string getName() const;
+		int getGrade() const;
+
+		// OTHER
+		void incrementGrade();
+		void decrementGrade();
+
+		// EXCEPTIONS
+		class GradeTooHighException: public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException: public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+	private:
+		Bureaucrat(); //Default Constructor
+		const std::string name_;
+		int grade_;
+
+		
+};
+
+// OUTSTREAM OPERATOR OVERLOAD
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& i);
+
+#endif
---
+++ cpp_05/ex01New/main.cpp
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmatheis <jmatheis@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 13:40:32 by jmatheis          #+#    #+#             */
+/*   Updated: 2023/05/11 09:40:40 by jmatheis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    {
+        std::cout << YELLOW "\nTEST: CONSTRUCTOR WITH GRADE TOO LOW" RESET << std::endl;
+        try
+        {
+            std::cout << "Grade: 151" << std::endl;
+            Bureaucrat("Bob1", 151);
+        }
+        catch(std::exception &b)
+        {
+            std::cout << RED << b.what() << RESET<< std::endl;
+        }   
+    }
+    {
+        std::cout << YELLOW "\nTEST: CONSTRUCTOR WITH GRADE TOO HIGH" RESET << std::endl;
+        try
+        {
+            std::cout << "Grade: 0" << std::endl;
+            Bureaucrat Bob2("Bob2", 0);
+        }
+        catch(std::exception &b)
+        {
+            std::cout << RED << b.what() << RESET<< std::endl;
+        } 
+    }
+    {
+        std::cout << YELLOW "\nTEST: INCREMENT GRADE -> TOO HIGH" RESET << std::endl;
+        Bureaucrat Bob3("Bob3", 2);
+        try
+        {
+            std::cout << Bob3 << std::endl;
+            Bob3.incrementGrade();
+            Bob3.incrementGrade();
+        }
+        catch(std::exception &b)
+        {
+            std::cout << Bob3 << std::endl;
+            std::cout << RED << b.what() << RESET<< std::endl;
+        }
+    }
+    {
+        std::cout << YELLOW "\nTEST: DECREMENTING GRADE -> TOO LOW" RESET << std::endl;
+        Bureaucrat *Bob4 = new Bureaucrat("Bob4", 150);
+        try
+        {
+            std::cout << *Bob4 << std::endl;
+            Bob4->decrementGrade();
+            Bob4->decrementGrade();
+            Bob4->decrementGrade();
+            Bob4->decrementGrade();
+            std::cout << "This should not get printed" << std::endl;
+        }
+        catch(std::exception &b)
+        {
+            std::cout << *Bob4 << std::endl;
+            std::cout << RED << b.what() << RESET<< std::endl;
+        }
+        delete Bob4;
+    }
+    {
+        std::cout << YELLOW "\nTEST: NO EXCEPTIONS" RESET << std::endl;
+        Bureaucrat *Bob5 = new Bureaucrat("Bob5", 100);
+        try
+        {
+            std::cout << *Bob5 << std::endl;
+            Bob5->decrementGrade();
+            Bob5->decrementGrade();
+            Bob5->decrementGrade();
+            Bob5->incrementGrade();
+        }
+        catch(std::exception &b)
+        {
+            std::cout << "This should not get printed" << std::endl;
+            std::cout << *Bob5 << std::endl;
+            std::cout << RED << b.what() << RESET<< std::endl;
+        }
+        std::cout << *Bob5 << std::endl;
+        delete Bob5;
+    }
+    return 0;
+}
