b6391394a406bb00a5ba68a9cfa8e7f28ce14623
Julia Ma <juliama@air-von-julia.speedport.ip>
2023-05-20 14:19:11 +0200

    cpp07 ex00 done

---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmatheis <jmatheis@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 10:42:14 by jmatheis          #+#    #+#             */
+/*   Updated: 2023/05/20 14:08:32 by jmatheis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+#define RESET       "\033[0m"               /* Reset */
+#define RED         "\033[31m"              /* Red */
+#define GREEN       "\033[32m"              /* Green */
+#define YELLOW      "\033[33m"              /* Yellow */
+#define PURPLE      "\033[35m"              /* Purple */
+
+template <typename T>
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b) {
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template <typename T>
+T max(T a, T b) {
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+#endif
