f74c1ea65bef58f936bcac888e58a89ecb69321b
Julia Ma <juliama@Air-von-Julia.fritz.box>
2023-05-26 11:20:09 +0200

    cpp08 ex00 done

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jmatheis <jmatheis@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 10:42:14 by jmatheis          #+#    #+#             */
-/*   Updated: 2023/05/25 15:36:39 by jmatheis         ###   ########.fr       */
+/*   Updated: 2023/05/26 11:18:50 by jmatheis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define WHATEVER_HPP
 
 # include <iostream>
+# include <vector>
+# include <list>
+# include <deque>
 
 #define RESET       "\033[0m"               /* Reset */
 #define RED         "\033[31m"              /* Red */
@@ -21,16 +24,22 @@
 #define YELLOW      "\033[33m"              /* Yellow */
 #define PURPLE      "\033[35m"              /* Purple */
 
+class NoOccurenceFound: public std::exception {
+	public:
+		virtual const char* what() const throw(){
+			return("Occurence not found in Container!");
+		}
+};
+
 template <typename T>
-int easyfind(T a, int i) {
-	std::list::const_iterator it = a.begin();
-	std::list::const_iterator it = a.find(i);
+int easyfind(T a, int i)
+{
+	typename T::const_iterator it = std::find(a.begin(), a.end(), i);
 	
-	while(it !=)
-	
-	// T tmp = a;
-	// a = b;
-	// b = tmp;
+	if(it != a.end())
+		return (*it);
+	else
+		throw NoOccurenceFound();
 }
 
 #endif
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,69 +6,100 @@
 /*   By: jmatheis <jmatheis@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 13:40:32 by jmatheis          #+#    #+#             */
-/*   Updated: 2023/05/25 13:06:39 by jmatheis         ###   ########.fr       */
+/*   Updated: 2023/05/26 11:19:28 by jmatheis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "whatever.hpp"
+#include "easyfind.hpp"
 
 int main(void)
 {
-    std::cout << YELLOW "INT TEST" RESET << std::endl;
-    int a = 2;
-    int b = 3;
-    ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-
-    std::cout << YELLOW "STRING TEST" RESET << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    
-    std::cout << YELLOW "CHAR TEST" RESET << std::endl;
-    char e = 'a';
-    char f = 'b';
-    ::swap(e, f);
-    std::cout << "e = " << e << ", f = " << f << std::endl;
-    std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
-    std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
-
+    {
+        std::cout << YELLOW "VECTOR TEST" RESET << std::endl;
+        std::vector<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        std::vector<int>::iterator itb = v.begin();
+        std::vector<int>::iterator ite = v.end();
+        while(itb != ite)
+        {
+            std::cout << *itb << std::endl;
+            itb++;
+        }
+        try
+        {
+            std::cout << PURPLE "Find int = 1" RESET << std::endl;
+            std::cout << easyfind(v, 1) << std::endl;
+            std::cout << PURPLE "Find int = 3" RESET << std::endl;
+            std::cout << easyfind(v, 3) << std::endl;
+            std::cout << PURPLE "Find int = 2" RESET << std::endl;
+            std::cout << easyfind(v, 2) << std::endl;
+            std::cout << PURPLE "Find int = 5" RESET << std::endl;
+            std::cout << easyfind(v, 5) << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << RED << e.what() << RESET << '\n';
+        }
+    }
+    {
+        std::cout << YELLOW "\nLIST TEST" RESET << std::endl;
+        std::list<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        std::list<int>::iterator itb = v.begin();
+        std::list<int>::iterator ite = v.end();
+        while(itb != ite)
+        {
+            std::cout << *itb << std::endl;
+            itb++;
+        }
+        try
+        {
+            std::cout << PURPLE "Find int = 1" RESET << std::endl;
+            std::cout << easyfind(v, 1) << std::endl;
+            std::cout << PURPLE "Find int = 3" RESET << std::endl;
+            std::cout << easyfind(v, 3) << std::endl;
+            std::cout << PURPLE "Find int = 2" RESET << std::endl;
+            std::cout << easyfind(v, 2) << std::endl;
+            std::cout << PURPLE "Find int = 5" RESET << std::endl;
+            std::cout << easyfind(v, 5) << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << RED << e.what() << RESET << '\n';
+        }
+    }    
+    {
+        std::cout << YELLOW "\nDEQUE TEST" RESET << std::endl;
+        std::deque<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        std::deque<int>::iterator itb = v.begin();
+        std::deque<int>::iterator ite = v.end();
+        while(itb != ite)
+        {
+            std::cout << *itb << std::endl;
+            itb++;
+        }
+        try
+        {
+            std::cout << PURPLE "Find int = 1" RESET << std::endl;
+            std::cout << easyfind(v, 1) << std::endl;
+            std::cout << PURPLE "Find int = 3" RESET << std::endl;
+            std::cout << easyfind(v, 3) << std::endl;
+            std::cout << PURPLE "Find int = 2" RESET << std::endl;
+            std::cout << easyfind(v, 2) << std::endl;
+            std::cout << PURPLE "Find int = 5" RESET << std::endl;
+            std::cout << easyfind(v, 5) << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << RED << e.what() << RESET << '\n';
+        }
+    }
     return 0;
 }
-
-// class Awesome{
-//     public:
-//         Awesome(void): _n(0){}
-//         Awesome(int n): _n(n){}
-//         Awesome& operator=(Awesome& a) {_n = a._n; return *this;}
-//         bool operator==(Awesome const & rhs) const {return (this->_n == rhs._n);}
-//         bool operator!=(Awesome const & rhs) const {return (this->_n != rhs._n);}
-//         bool operator>(Awesome const & rhs) const {return (this->_n > rhs._n);}
-//         bool operator<(Awesome const & rhs) const {return (this->_n < rhs._n);}
-//         bool operator>=(Awesome const & rhs) const {return (this->_n >= rhs._n);}
-//         bool operator<=(Awesome const & rhs) const {return (this->_n <= rhs._n);}
-
-//         int get_n() const {return _n;}
-        
-//     private:
-//         int _n;
-// };
-
-// std::ostream& operator<<(std::ostream& o, const Awesome &a) {o << a.get_n(); return o;}
-
-
-// int main()
-// {
-//     Awesome a(2), b(4);
-
-//     swap(a, b);
-//     std::cout << a << " " << b << std::endl;
-//     std::cout << max(a,b) << std::endl;
-//     std::cout << min(a,b) << std::endl;
-//     return (0);
-// }
