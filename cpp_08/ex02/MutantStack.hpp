844762eaac31825c27f906e94fa77b7c5f58f8f7
Julia Ma <juliama@MacBook-Air-von-Julia.local>
2023-06-08 12:39:20 +0200

    cpp08 ex02

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jmatheis <jmatheis@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 10:42:14 by jmatheis          #+#    #+#             */
-/*   Updated: 2023/06/02 17:17:26 by jmatheis         ###   ########.fr       */
+/*   Updated: 2023/06/08 12:39:02 by jmatheis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,28 @@ class MutantStack : public std::stack<T>
 		const_iterator end() const {return(this->c.end());}
 };
 
+
+// typedef: assigning an alternative name to  a specific type
+// typename: used to tell compiler
+// that std::stack<T>::container_type::iterator
+// is a valid type and should be named as iterator.
+// without typename: compiler would not recognize iterator as type automatically
+// -> typename helps compiler to understand the right context
+// and to assign the right type
+
+// std::stack = a container adapter class
+// -> takes another existing container as base
+// which provides a LIFO (Last-In-First-Out) Data structure
+// the container used as base can be accessed via
+// the container_type member type of the std::stack class 
+// container_type = an alias for the underlying container
+// which is by default a std::deque
+
+// underlying container stores every information of the stack elements
+// -> it's the base
+// usually deque if there is no other container type stated explicitly
+// Ein Beispiel dafür wäre std::stack<int, std::vector<int>>,
+// wobei std::vector<int> als der zugrundeliegende Container verwendet wird
+// c stand for the underlying container -> container_type
+// c.begin() is equal to std::stack<T>::container_type::begin()
 #endif
